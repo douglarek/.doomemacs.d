21caf091c59c8125132769ec6ecd1248b33202a7
douglarek <douglarek@gmail.com>
2023-06-07 17:15:44 +0800

    Remove `Package cl is deprecated` warning

--- config.el
+++ config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(setq byte-compile-warnings '(cl-functions))
+
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "John Doe"
